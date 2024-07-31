4ba83facaf0c92c58e8efcd71774d2e4b20587c8
Nitturu Thanisha <nitturuthanisha@gmail.com>
2024-07-31 15:36:30 +0545

    Updates

--- src/module.mjs
+++
@@ -1,4 +0,0 @@
-import sc,{b,c} from "./module2.mjs"
-console.log(sc)
-console.log(b)
-console.log(c)
--- src/module2.mjs
+++
@@ -1,6 +0,0 @@
-const a="harry";
-const b="potter";
-const c="hi"
-export default a;
-export {b};
-export {c};
